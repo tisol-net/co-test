18ca20c579bb458d921a47166d5651abd5f6a361
Peter Taylor <peter.taylor@tisol.net>
2024-02-22 14:00:39 +0000

    Removing block

--- variables.tf
+++ variables.tf
@@ -1,10 +1,3 @@
-accounts = {
-  live    = "178668284513"
-  asur    = "910873820060"
-  test    = "644536951056"
-  staging = "539477383635"
-}
-
 variable accounts {
   description = "Account IDs for environment accounts"
   type = object(
