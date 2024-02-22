c158e91862c9773aa614ac31b61ae3218c2f2d92
Peter Taylor <peter.taylor@tisol.net>
2024-02-22 14:01:33 +0000

    Correcting dictionary

--- variables.tf
+++ variables.tf
@@ -2,10 +2,7 @@ variable accounts {
   description = "Account IDs for environment accounts"
   type = object(
     {
-      live    = string
-      asur    = string
-      test    = string
-      staging = string
+      dev = string
     }
   )
 }
